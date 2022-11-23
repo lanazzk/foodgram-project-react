7fb51b54b0ab5e6aa11e281e4a9aecee53da0966
Svetlana Ammosova <diakonovase@gmail.com>
2022-11-23 17:05:00 +0100

    fix users view

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
 
-from .views import UserDetail, UserList
+# from .views import UserDetail, UserList
 
 app_name = 'users'
 
 
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
+    # path('users/', UserList.as_view()),
+    # path('users/<int:pk>/', UserDetail.as_view()),
     path('', include('djoser.urls')),
 ]
--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,25 +1,35 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+# from rest_framework import generics
+from rest_framework import viewsets
 
 from .serializers import CurrentUserSerializer, UserPostSerializer
 
 User = get_user_model()
 
 
-class UserList(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
+    serializer_class = CurrentUserSerializer
 
     def get_serializer_class(self):
         if self.request.method in ('POST'):
             return UserPostSerializer
         return CurrentUserSerializer
 
+# class UserList(generics.ListCreateAPIView):
+#     queryset = User.objects.all()
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = CurrentUserSerializer
+#     def get_serializer_class(self):
+#         if self.request.method in ('POST'):
+#             return UserPostSerializer
+#         return CurrentUserSerializer
 
 
-class UserCreate(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserPostSerializer
+# class UserDetail(generics.RetrieveAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = CurrentUserSerializer
+
+
+# class UserCreate(generics.CreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserPostSerializer
