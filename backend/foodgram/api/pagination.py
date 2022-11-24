178ac98c8c661c2a96e81b86f4a4306374d14548
Svetlana Ammosova <diakonovase@gmail.com>
2022-11-24 01:15:00 +0100

    текст комментария

--- backend/foodgram/api/pagination.py
+++ backend/foodgram/api/pagination.py
@@ -3,4 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class RecipePagination(PageNumberPagination):
     page_size = 6
-    page_size_query_param = 999
+    page_size_query_param = 'limit'
